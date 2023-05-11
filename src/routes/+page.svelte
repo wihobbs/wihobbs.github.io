c0181a1e7166a438b2740297ac48fe76ebaef2a1
William Hobbs <13199119+wihobbs@users.noreply.github.com>
2023-05-11 10:26:55 -0400

    updating before wipe

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>The world of Hobbs</h1>
+<p> <a href="/about">about</a></p>
+<p> <a href="/projects">projects</a></p>
+<p> <a href="/travels">travels</a></p>
+<p> <a href="/blog">blog</a></p>
+<p> <a href="/contact">contact me</a></p>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,3 @@
+<h1 class="center">
+    About Me
+</h1>
